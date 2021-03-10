906251557fc37db046f5e175fb5bc06a4cc475ee
Daniel Silva <daniel.raizon@gmail.com>
2021-03-10 15:10:09 -0300

    Generate and use JWT Token.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
 
 import User from '../models/User';
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
 	user: User;
+	token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination.');
 		}
 
+		const token = sign({}, '9e2ace06b77f9e03ba43a84d00519569', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
 		return {
 			user,
+			token,
 		};
 	}
 }
